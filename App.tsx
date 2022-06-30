203b25c8284c1b89672be62909f5d55854e9547b
Cesar <cesar.villarreal@mail.utoronto.ca>
2022-06-30 14:10:26 +0000

    First test of tunnel connection and some basic code

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar"
+import AppContainer from "./src/components/app-container"
+import MainScreen from "./src/screens/main"
+
+export default function App() {
+  return (
+    <AppContainer>
+      <MainScreen />
+    </AppContainer>
+  )
+}
